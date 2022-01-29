653a65d23349b48e2346a6954f7b47a7cfe2de52
Smile Gupta <guptamiley3012@gmail.com>
2022-01-30 00:30:28 +0530

    Syncing Dev with Master (#37)
    * Feat: Added the styles for the footer  (#31)
    
    * (styles): Enhanced CSS Styles
    
    * (styles): Enhanced the styles for the footer
    
    * Feat: Added Navbar for the component library (#32)
    
    * (refactor): Code refactor done
    
    * (feat): Basic Navbar added
    
    * (feat):Added Sidebar
    
    * Feat Homepage: Added Styles for homepage and completed routing (#34)
    
    * (feat): Added basic design for the home page
    
    * (feat): Adding Links
    
    * (feat): Added Code Componnet
    
    * (feat): Basic Routing Setup Done
    
    * (preetify): Preetified the component
    
    * Feat: Documentation page setup (#36)
    
    * (refactor): Enhanced style names and added missed routing
    
    * (feat): Basic Sidebar added
    
    * (styles): Added styles and functionality to sidebar
    
    * (refactor): Doc Site Styles
    
    * (feat): Setup of docmentation page

---
+++ assets/images.js
@@ -0,0 +1,7 @@
+const images = [
+  "https://image-dock-uploads-be.s3.ap-south-1.amazonaws.com/image.2022-01-29T18%3A49%3A51.418Z",
+  "https://image-dock-uploads-be.s3.ap-south-1.amazonaws.com/image.2022-01-29T18%3A45%3A22.573Z",
+  "https://image-dock-uploads-be.s3.ap-south-1.amazonaws.com/image.2022-01-29T18%3A51%3A31.435Z",
+  "https://image-dock-uploads-be.s3.ap-south-1.amazonaws.com/image.2022-01-29T18%3A49%3A36.120Z",
+  "https://image-dock-uploads-be.s3.ap-south-1.amazonaws.com/image.2022-01-29T18%3A36%3A20.918Z",
+];
